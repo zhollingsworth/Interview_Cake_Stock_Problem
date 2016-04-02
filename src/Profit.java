98bbf4a81abb966427a924e98077e631b162df59
Zac Hollingsworth <zahollingsworth@valdosta.edu>
2016-04-02 14:32:15 -0400

    Altered code to deal with earlier unknown issue as well as to deal with the case of negative profits.

--- src/Profit.java
+++ src/Profit.java
@@ -1,4 +1,4 @@
-/*****************************************************************
+/********************************************************************************************************************************
 Zac Hollingsworth
 4/2/2016
 
@@ -12,34 +12,35 @@ The indices are the time in minutes past trade opening time, which was 9:30am lo
 The values are the price in dollars of Apple stock at that time.
 So if the stock cost $500 at 10:30am, stockPricesYesterday[60] = 500.
 
-Write an efficient function that takes stockPricesYesterday and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
+Write an efficient function that takes stockPricesYesterday and returns the best profit I 
+could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
 
 No "shorting"—you must buy before you sell. You may not buy and sell in the same time step (at least 1 minute must pass).
-*******************************************************************/
+********************************************************************************************************************************/
 
 public class Profit {
 	
 	public static int getMaxProfit(int[] a)
 	{
-		int  max = 0;
-		int trueMax = 0;
-		int  min = 0;
+		int  max = -2147483648;
 		
-		for(int i = 1; i < a.length; i++)
+		for(int i = 0; i < a.length; i++)
 		{
-			max = (a[max] < a[i] && (i < min)) ? i:max;
-			trueMax = (a[trueMax] < a[i]) ? i:trueMax;
-			min = (a[i] < a[min]) ? i:min;
+			for(int j = i+1; j < a.length; j++)
+			{
+				max = ((a[j] - a[i]) > max) ? (a[j] - a[i]) : max;
+			}
 		}
-		max = ((a[trueMax] > a[max]) && (trueMax < min)) ? trueMax:max;
-		return a[max] - a[min];
+		return max;
 	}
 	
 	public static void main(String[] args)
 	{
 		int[] stockPricesYesterday = new int[]{10,7,5,8,11,4,9};
 		int[] stockPricesYesterday2 = new int[]{10,7,5,8,4,11,4,9};
+		int[] stockPricesYesterday3 = new int[]{20,15,11,10,8,2};
 		System.out.println("Best profit = "+getMaxProfit(stockPricesYesterday));
 		System.out.println("Best profit = "+getMaxProfit(stockPricesYesterday2));
+		System.out.println("Best profit = "+getMaxProfit(stockPricesYesterday3));
 	}
 }
